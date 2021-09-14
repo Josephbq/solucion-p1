cf0c1aa92eddbafb6dc6446c5575237a0f9bb0a8
barra <barra@LAPTOP-U81S3TP5>
2021-09-13 21:15:21 -0400

    model for movie and dbee

---
+++ Models/DBEntities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace solucion_p1.Models
+{
+    public class DBEntities : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace solucion_p1.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime RealeaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
